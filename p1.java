9a079e48aa04e5f9f9a4fa4ff4a02b92b4663359
anjan <anjankumarsahoo1097@gmail.com>
2025-04-22 15:18:40 +0530

    init2

--- p1.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class p1 {
-    public static void main(String[] args) {
-        String[] cities = {"Pune","Rourkela","Bhubaneswar","Mumbai","Delhi"};
-            for(int i=0;i<cities.length;i++)
-                if(cities[i].equals("Mumbai"))
-                    System.out.println("Mumbai is at index "+i);
-    }
-}
